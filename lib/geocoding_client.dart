116e8cb94308c7d073990ee6e3333e02ffe8bd63
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2023-08-06 17:10:00 +0900

    test

--- lib/geocoding_client.dart
+++ lib/geocoding_client.dart
@@ -1,13 +1,7 @@
 library geocoding_client;
 
 export 'package:geocoding_client_interface/geocoding_client_interface.dart'
-    show GeocodingClient, PlaceMark;
-
-class Hoge {
-  const Hoge({this.name = 'hoge'});
-  final String name;
-
-  void printName() {
-    print(name);
-  }
-}
+    show GeocodingClient;
+export 'package:geocoding_client_interface/place_mark.dart' show PlaceMark;
+export 'package:geocoding_client_interface/not_found_geocoding_api_place.dart'
+    show NotFoundGeocodingApiPlace;
