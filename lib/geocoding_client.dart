b441d6e7cb1052d4f80c4f13f1c124f68dec01a3
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2024-05-18 15:47:51 +0900

    fix: flutterとパッケージのバージョンを更新

--- lib/geocoding_client.dart
+++ lib/geocoding_client.dart
@@ -1,7 +1,7 @@
 library geocoding_client;
 
 export 'package:geocoding_client_interface/geocoding_client_interface.dart'
-    show GeocodingClient, GeocodingClientDefault;
+    show GeocodingClient;
 export 'package:geocoding_client_interface/place_mark.dart' show PlaceMark;
 export 'package:geocoding_client_interface/not_found_geocoding_api_place.dart'
     show NotFoundGeocodingApiPlace;
